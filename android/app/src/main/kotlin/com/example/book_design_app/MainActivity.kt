e21d2e992f6f6bf847adcdc94c8cb1c41613c3ae
Tommybarral <isamutom27@hotmail.com>
2022-05-09 10:51:42 +0200

    starting project

---
+++ android/app/src/main/kotlin/com/example/book_design_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.book_design_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/screens/welcome_screen.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class WelcomeScreen extends StatefulWidget {
+  const WelcomeScreen({Key? key}) : super(key: key);
+  @override
+  State<WelcomeScreen> createState() => _WelcomeScreenState();
+}
+
+class _WelcomeScreenState extends State<WelcomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
